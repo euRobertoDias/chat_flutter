16e8def0a46125cafeeda5db4d8f5a3500c130a1
hombrous <hombrous@gmail.com>
2020-04-22 10:02:33 -0300

    modificando um pouco

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -28,7 +28,9 @@ class _ChatScreenState extends State<ChatScreen> {
       super.initState();
 
       FirebaseAuth.instance.onAuthStateChanged.listen((user) {
-        _currentUser = user;
+        setState(() {
+          _currentUser = user;
+        });
       });
     }
     
@@ -88,10 +90,27 @@ class _ChatScreenState extends State<ChatScreen> {
     }
 
     return Scaffold(
+      
       key: _scaffoldKey,
       appBar: AppBar(
-        title: Text('Olá'),
+        title: Text(
+          _currentUser != null ? 'Olá, ${_currentUser.displayName}' : 'Chat app'
+        ),
         elevation: 0,
+        actions: <Widget>[
+          _currentUser != null ? IconButton(
+            icon: Icon(Icons.exit_to_app),
+            onPressed: (){
+              FirebaseAuth.instance.signOut();
+              googleSignIn.signOut();
+              _scaffoldKey.currentState.showSnackBar(
+                SnackBar(
+                  content: Text('Você saiu com sucesso'),
+                )
+              );
+            },
+          ) : Container()
+        ],
       ),
       body: Column(
         children: <Widget>[
